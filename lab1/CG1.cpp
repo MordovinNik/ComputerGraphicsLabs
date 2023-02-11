56ca42e36cba12e7199f4d640883a9826ce6c94c
MordovinNik <62290470+MordovinNik@users.noreply.github.com>
2023-02-11 16:57:17 +0300

    lab1 done

---
+++ lab1/CG1.cpp
@@ -0,0 +1,358 @@
+﻿// CG1.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "CG1.h"
+#include <d3d11.h>
+#include <dxgi.h>
+#include <assert.h>
+#include <exception>
+#pragma comment(lib, "dxgi")
+#pragma comment(lib, "d3d11")
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+HWND gHWnd = NULL;
+UINT m_width = 1280;
+UINT m_height = 720;
+
+ID3D11Device* m_pDevice = NULL;
+ID3D11DeviceContext* m_pDeviceContext = NULL;
+IDXGISwapChain* m_pSwapChain = NULL;
+ID3D11RenderTargetView* m_pBackBufferRTV = NULL;
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: Разместите код здесь.
+
+	// Инициализация глобальных строк
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_CG1, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Выполнить инициализацию приложения:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CG1));
+
+	MSG msg;
+
+	// Цикл основного сообщения:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+			m_pDeviceContext->ClearState();
+
+			ID3D11RenderTargetView* views[] = { m_pBackBufferRTV };
+			m_pDeviceContext->OMSetRenderTargets(1, views, nullptr);
+			static const FLOAT BackColor[4] = { 0.75f, 0.75f, 0.25f, 1.0f };
+			m_pDeviceContext->ClearRenderTargetView(m_pBackBufferRTV, BackColor);
+
+			HRESULT hr = m_pSwapChain->Present(0, 0);
+			assert(SUCCEEDED(hr));
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CG1));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_CG1);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	RECT rc;
+	rc.left = 0;
+	rc.right = m_width;
+	rc.top = 0;
+	rc.bottom = m_height;
+
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, TRUE);
+
+	MoveWindow(hWnd, 0, 0, rc.right - rc.left, rc.bottom - rc.top, TRUE);
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	gHWnd = hWnd;
+
+	HRESULT hr;
+
+	IDXGIFactory* pFactory = NULL;
+	hr = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&pFactory);
+	assert(SUCCEEDED(hr));
+
+	IDXGIAdapter* pSelectedAdapter = NULL;
+
+	if (SUCCEEDED(hr))
+	{
+		IDXGIAdapter* pAdapter = NULL;
+		UINT adapterIdx = 0;
+
+		while (SUCCEEDED(pFactory->EnumAdapters(adapterIdx, &pAdapter)))
+		{
+			DXGI_ADAPTER_DESC desc;
+
+			pAdapter->GetDesc(&desc);
+
+			if (wcscmp(desc.Description, L"Microsoft Basic Render Driver") != 0)
+			{
+				pSelectedAdapter = pAdapter;
+				break;
+			}
+
+			pAdapter->Release();
+			adapterIdx++;
+		}
+	}
+	assert(pSelectedAdapter != NULL);
+
+	D3D_FEATURE_LEVEL level;
+	D3D_FEATURE_LEVEL levels[] = { D3D_FEATURE_LEVEL_11_0 };
+
+	if (SUCCEEDED(hr))
+	{
+		UINT flags = 0;
+#ifdef _DEBUG
+		flags = D3D11_CREATE_DEVICE_DEBUG;
+#endif // _DEBUG
+
+		hr = D3D11CreateDevice(pSelectedAdapter, D3D_DRIVER_TYPE_UNKNOWN, NULL,
+			flags, levels, 1, D3D11_SDK_VERSION, &m_pDevice, &level, &m_pDeviceContext);
+		assert(level == D3D_FEATURE_LEVEL_11_0);
+		assert(SUCCEEDED(hr));
+
+	}
+
+	if (SUCCEEDED(hr))
+	{
+		DXGI_SWAP_CHAIN_DESC swapChainDesc = { 0 };
+		swapChainDesc.BufferCount = 2;
+		swapChainDesc.BufferDesc.Width = m_width;
+		swapChainDesc.BufferDesc.Height = m_height;
+		swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		swapChainDesc.BufferDesc.RefreshRate.Numerator = 0;
+		swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+		swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		swapChainDesc.OutputWindow = hWnd;
+		swapChainDesc.SampleDesc.Count = 1;
+		swapChainDesc.SampleDesc.Quality = 0;
+		swapChainDesc.Windowed = true;
+		swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+		swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+		swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+		swapChainDesc.Flags = 0;
+
+		hr = pFactory->CreateSwapChain(m_pDevice, &swapChainDesc, &m_pSwapChain);
+		assert(SUCCEEDED(hr));
+	}
+
+	if (SUCCEEDED(hr))
+	{
+		ID3D11Texture2D* pBackBuffer = NULL;
+		hr = m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+		assert(SUCCEEDED(hr));
+		if (SUCCEEDED(hr))
+		{
+			hr = m_pDevice->CreateRenderTargetView(pBackBuffer, NULL, &m_pBackBufferRTV);
+			assert(SUCCEEDED(hr));
+
+			if (pBackBuffer != NULL)
+			{
+				pBackBuffer->Release();
+				pBackBuffer = NULL;
+			}
+		}
+	}
+
+	if (pSelectedAdapter != NULL)
+	{
+		pSelectedAdapter->Release();
+		pSelectedAdapter = NULL;
+	}
+
+	if (pFactory != NULL)
+	{
+		pFactory->Release();
+		pFactory = NULL;
+	}
+
+	return SUCCEEDED(hr);
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_SIZE:
+	{
+		RECT rc;
+		GetClientRect(hWnd, &rc);
+
+		UINT width = rc.right - rc.left;
+		UINT height = rc.bottom - rc.top;
+
+		if (width != m_width || height != m_height) {
+			if (m_pBackBufferRTV != NULL)
+			{
+				m_pBackBufferRTV->Release();
+				m_pBackBufferRTV = NULL;
+			}
+
+			HRESULT hr = m_pSwapChain->ResizeBuffers(2, width, height, DXGI_FORMAT_R8G8B8A8_UNORM, 0);
+
+			if (SUCCEEDED(hr)) {
+				m_width = width;
+				m_height = height;
+
+				ID3D11Texture2D* pBackBuffer = NULL;
+				hr = m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+				if (SUCCEEDED(hr)) {
+					hr = m_pDevice->CreateRenderTargetView(pBackBuffer, NULL, &m_pBackBufferRTV);
+
+					if (pBackBuffer != NULL)
+					{
+						pBackBuffer->Release();
+						pBackBuffer = NULL;
+					}
+				}
+
+				assert(SUCCEEDED(hr));
+			}
+		}
+
+		break;
+	}
+
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Разобрать выбор в меню:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: Добавьте сюда любой код прорисовки, использующий HDC...
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
